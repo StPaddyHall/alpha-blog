36f17f591ca484c08596517229620ab6bbb05cad
Patrick Hall <artificial.kulture@gmail.com>
2017-01-17 08:35:26 +0000

    add pagination to views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
     
     def index
         # plural as we are grabbing all
-        @articles = Article.all
+        @articles = Article.paginate(page: params[:page], per_page: 5)
     end
     
     def new
